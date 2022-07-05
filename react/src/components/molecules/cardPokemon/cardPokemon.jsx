7088086128aaa7d9a43843101834777dc1afb910
Bruno <brunoowap@gmail.com>
2022-07-05 18:29:39 -0300

    adicionando dados adicionais dos pokemons  no banco json

--- react/src/components/molecules/cardPokemon/cardPokemon.jsx
+++ react/src/components/molecules/cardPokemon/cardPokemon.jsx
@@ -1,7 +1,12 @@
-export default function listPokedex() {
+export default function listPokedex(props) {
   return (
     <section>
       <h1>Card</h1>
+      {props.dados.map((dado) => (
+        <div>
+          <h2>{dado.name}</h2>
+        </div>
+      ))}
     </section>
   );
 }
