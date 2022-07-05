c142ec28a58cb23fdfe62e6ac878f4a9987eb5cd
Bruno <brunoowap@gmail.com>
2022-07-05 18:44:11 -0300

    criando a pasta public e adicionando a pasta de imagens nela, também exibindo a pokedex

--- react/src/components/organisms/listPokedex/listPokedex.jsx
+++ react/src/components/organisms/listPokedex/listPokedex.jsx
@@ -1,9 +1,11 @@
 import CardPokemon from "../../molecules/cardPokemon/cardPokemon";
+import PokemonsList from "../../../data/data.json";
+
 export default function listPokedex() {
   return (
     <section>
       <h1>POKEDEX</h1>
-      <CardPokemon />
+      <CardPokemon dados={PokemonsList} />
     </section>
   );
 }
