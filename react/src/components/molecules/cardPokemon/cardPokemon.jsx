0f99a6da37bb18371454ff466f9da16ef2cf2861
Bruno <brunoowap@gmail.com>
2022-07-05 18:54:43 -0300

    Iniciando o css dos card dos pokemons

--- react/src/components/molecules/cardPokemon/cardPokemon.jsx
+++ react/src/components/molecules/cardPokemon/cardPokemon.jsx
@@ -1,14 +1,14 @@
+import "./index.scss";
 export default function listPokedex(props) {
   return (
-    <section>
-      <h1>Card</h1>
+    <section className="container">
       {props.dados.map((dado) => (
-        <div key={dado.id}>
+        <div className="container-card" key={dado.id}>
           <section>
             <h2>{dado.name}</h2>
             <span>{dado.type}</span>
           </section>
-          <img src={dado.img} />
+          <img className="container-imgPoke" src={dado.img} />
         </div>
       ))}
     </section>
