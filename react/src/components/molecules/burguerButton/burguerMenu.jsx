a821425ae2d2f1f95b2d3d5db22c5404f7745fca
Bruno <brunoowap@gmail.com>
2022-07-06 16:08:54 -0300

    Adicionando estilo no burguerButton

--- react/src/components/molecules/burguerButton/burguerMenu.jsx
+++ react/src/components/molecules/burguerButton/burguerMenu.jsx
@@ -1,13 +1,20 @@
 import { burguerAction } from "../../../services/ui-burguer";
 import { useDispatch } from "react-redux";
+
+import burguerIcon from "/assets/img/burguerIcon.png";
+
+import "./burguerButton.scss";
+
 export default function burguerMenu() {
   const dispath = useDispatch();
   const toggleBurguerHandler = () => {
     dispath(burguerAction.toggle());
   };
   return (
-    <button onClick={toggleBurguerHandler}>
-      <span>Burguer Menu</span>
+    <button onClick={toggleBurguerHandler} className="burguer">
+      <span>
+        <img src={burguerIcon} />
+      </span>
     </button>
   );
 }
