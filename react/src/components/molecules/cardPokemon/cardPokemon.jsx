1d30a4a9dca6256565510ee78cfa35b7ec41457d
Bruno <brunoowap@gmail.com>
2022-07-08 14:33:02 -0300

    consumindo poke api e exibindo na tela

--- react/src/components/molecules/cardPokemon/cardPokemon.jsx
+++ react/src/components/molecules/cardPokemon/cardPokemon.jsx
@@ -22,18 +22,6 @@ export default function listPokedex(props) {
   return (
     <section className="container">
       <ul>{maped}</ul>
-
-      {/*   {props.dados.map((dado) => (
-        <div className="container-card" key={dado.id}>
-          <section>
-            <h2>{dado.name}</h2>
-            <span>{dado.type}</span>
-          </section>
-          <div>
-            <img className="container-imgPoke" src={dado.img} />
-          </div>
-        </div>
-      ))} */}
     </section>
   );
 }
