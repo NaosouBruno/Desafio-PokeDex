885211d4b3417564559857457ae6bbe1f187f0fe
Bruno <brunoowap@gmail.com>
2022-07-15 12:52:53 -0300

    fazendo componente estatico evolution

--- react/src/components/molecules/evolution/evolution.jsx
+++ react/src/components/molecules/evolution/evolution.jsx
@@ -1,12 +1,44 @@
 import "./evolution.scss";
-import PokeEvo from "../../../../public/svg/pokeEvo.jsx";
+import EvoIcon from "../../../../public/svg/evoIcon.svg";
+import RightArrow from "/assets/img/rightArrow.png";
 import Bulba from "/assets/img/bulba.png";
+import Bulba2 from "../../../../public/svg/bulba2.svg";
+import Bulba3 from "../../../../public/svg/bulba3.svg";
+import Bulba4 from "../../../../public/svg/bulba4.svg";
 export default function Evolution() {
   return (
-    <div>
-      <h1>Evolution Chain</h1>
-      <PokeEvo />
-      <img src={Bulba} />
+    <div className="evolution">
+      <h1 className="evolution-title">Evolution Chain</h1>
+      <div className="evolution-container">
+        <div className="evolution-faseOne">
+          <img src={EvoIcon} className="evolution-pokeIcon" />
+          <img src={Bulba} className="evolution-pokeSelf" />
+        </div>
+        <div className="evolution-containerLevel">
+          <img src={RightArrow} />
+          <span className="evolution-pokeLevel">Lvl 16</span>
+        </div>
+        <div className="evolution-faseOne">
+          <img src={EvoIcon} className="evolution-pokeIcon" />
+          <img src={Bulba2} className="evolution-pokeSelf" />
+        </div>
+      </div>
+      <hr></hr>
+
+      <div className="evolution-container">
+        <div className="evolution-faseOne">
+          <img src={EvoIcon} className="evolution-pokeIcon" />
+          <img src={Bulba3} className="evolution-pokeSelf" />
+        </div>
+        <div className="evolution-containerLevel">
+          <img src={RightArrow} />
+          <span className="evolution-pokeLevel">Lvl 34</span>
+        </div>
+        <div className="evolution-faseOne">
+          <img src={EvoIcon} className="evolution-pokeIcon" />
+          <img src={Bulba4} className="evolution-pokeSelf" />
+        </div>
+      </div>
     </div>
   );
 }
